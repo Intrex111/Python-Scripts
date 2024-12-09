b37ae8ce75e8c71b7e76aff8a46f0078980a18a7
Intrex111 <intrex0000@gmail.com>
2024-12-09 10:55:28 +0300

    this is test to learn git

---
+++ testing git.py
@@ -0,0 +1,7 @@
+print("somethingsomething")
+#this is a comment and a tester for my first time using git
+x = 3
+if x > 
+    print("x is greater than 5")
+    else
+        print("x is smaller than 5")
